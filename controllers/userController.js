01de788e2cd862a4bdac2cf9c603b8543a3e4dc9
Jonnvoo <Jonathanvu2065@gmail.com>
2022-12-06 11:31:25 -0800

    user Routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,44 +2,66 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-const {User} = require('../models');
+const { User } = require('../models');
 
-
-router.post('/login', (req, res) => {
-    User.findOne({
-      where:{
-        email:req.body.email 
-      }
-    }).then(foundUser => {
-      if(!foundUser){
-        return res.status(401).json({msg:"Your email or password is incorrect!"});
-      }else if(!bcrypt.compareSync(req.body.password, foundUser.password)){
-        return res.status(401).json({msg:"Your email or password is incorrect!" });
-      } else {
-        const token = jwt.sign({
-            id:foundUser.id,
-            email:foundUser.email
-        },process.env.JWT_SECRET,{
-            expiresIn:"2h"
-        })
-      };
-    })
-    return res.json({
-        token,
-        user:foundUser
-    })
-     
+router.get("/", (req, res) => {
+    User.findAll({
+    }).then(users => {
+        res.json(users);
+    }).catch(err => {
+        res.status(500).json({ msg: "Can't find the users." });
     });
+});
 
-    router.post("api/users/signup", (req,res) => {
-        User.create({
-          email:req.body.email,
-          password:req.body.password,
-        }).then(newUser => {  
-          req.session.user_id=newUser.id;
-          req.session.logged_in=true;
-          res.json(newUser);
-        }).catch(err => {
-          res.status(500).json({msg: "Unable to create an account."});
-        });
-      });
+
+router.get("/getuserfromtoken",(req,res)=>{
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const userData = jwt.verify(token,process.env.JWT_SECRET)
+        res.json({user:userData})
+    } catch (error) {
+        res.status(500).json({user:false})
+    }
+})
+
+router.post("api/users/signup", (req, res) => {
+    User.create(req.body).then(newUser => {
+        const token = jwt.sign({
+            id: newUser.id,
+            email: newUser.email
+        }, process.env.JWT_SECRET, {
+            expiresIn: "2h"
+        })
+        return res.json({
+            token,
+            user: newUser
+        })
+    })
+})
+
+
+router.post("/login", (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(foundUser => {
+        if (!foundUser) {
+            return res.status(401).json({ msg: "Your email or password is incorrect!" });
+        } else if (!bcrypt.compareSync(req.body.password, foundUser.password)) {
+            return res.status(401).json({ msg: "Your email or password is incorrect!" });
+        } else {
+            const token = jwt.sign({
+                id: foundUser.id,
+                email: foundUser.email
+            }, process.env.JWT_SECRET, {
+                expiresIn: "2h"
+            })
+            return res.json({
+                token,
+                user: foundUser
+            })
+        }
+    })
+})
+
