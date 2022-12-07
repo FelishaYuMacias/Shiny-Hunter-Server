22c9e99ba65c4a6b5d7198922d59abd8911b6b2a
tkmarsten <tkmarsten@gmail.com>
2022-12-07 14:12:41 -0800

    Add CORS package

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./controllers');
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(cors())
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
