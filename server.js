c396897820e934146359cef35e266e7df757fb35
Felisha <felisha.j.yu@gmail.com>
2022-12-09 14:07:52 -0800

    app.options

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const app = express();
 
 app.use(cors({
   origin: /(https:\/\/shiny-hunter-server\.herokuapp\.com)\/(.*)$/
+  // origin: 'http://localhost:3000'
 }))
-
+app.options('*', cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
