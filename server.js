94a1745a3bbf02ab96a92bfc327a4e5665714ae5
Felisha <felisha.j.yu@gmail.com>
2022-12-09 13:42:47 -0800

    updated cors origin

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors({
-  origin: 'https://shiny-hunter-server.herokuapp.com'
+  origin: /(https:\/\/shiny-hunter-server\.herokuapp\.com)\/(.*)$/
 }))
 
 app.use(express.urlencoded({ extended: true }));
